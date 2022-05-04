ee091da2e6d52bb0ee86416b84c5db8d129a2043
kapatiuska <65649227+jordicapedo@users.noreply.github.com>
2022-05-04 11:32:39 +0200

    support dark theme

--- components/navbar.js
+++ components/navbar.js
@@ -14,7 +14,8 @@ import {
   IconButton,
   useColorModeValue
 } from '@chakra-ui/react'
-import { HamburgerIcon } from '@chakra-ui/icons'
+import { HamburgerIcon, GithubIcon } from '@chakra-ui/icons'
+import ThemeToggleButton from './theme-toggle-button'
 
 const LinkItem = ({ href, path, children }) => {
   const active = path === href
@@ -72,6 +73,7 @@ const Navbar = (props) => {
           </LinkItem>
         </Stack>
         <Box flex={1} align="right">
+          <ThemeToggleButton />
           <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
             <Menu>
               <MenuButton
@@ -79,7 +81,26 @@ const Navbar = (props) => {
                 icon={<HamburgerIcon />}
                 variant="outline"
                 aria-label="Options"
-              ></MenuButton>
+              />
+              <MenuList>
+                <NextLink href="/" passHref>
+                  <MenuItem as={Link}>About</MenuItem>
+                </NextLink>
+                <NextLink href="/works" passHref>
+                  <MenuItem as={Link}>Works</MenuItem>
+                </NextLink>
+                <NextLink href="/posts" passHref>
+                  <MenuItem as={Link}>Posts</MenuItem>
+                </NextLink>
+                <NextLink href="/posts" passHref>
+                  <MenuItem
+                    as={Link}
+                    href="https://github.com/jordicapedo/jordidev-homepage"
+                  >
+                    View Source
+                  </MenuItem>
+                </NextLink>
+              </MenuList>
             </Menu>
           </Box>
         </Box>
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '../components/layouts/main'
+import theme from '../lib/theme'
+import Fonts from '../components/fonts'
 
 const Website = ({ Component, pageProps, router }) => {
   return (
-    <ChakraProvider resetCSS>
+    <ChakraProvider theme={theme}>
+      <Fonts />
       <Layout router={router}>
         <Component {...pageProps} key={router.route} />
       </Layout>
